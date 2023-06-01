7089cf1d53a1aca67eb98ed0a60e2b667f886b2d
justtfelix <justttfelix@gmail.com>
2023-06-01 20:20:38 +0400

    [Client] Added two styled components and roboto font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { Helmet } from 'react-helmet-async';
 import { GlobalStyles } from './assets';
 import { router } from './routes';
 import { theme } from './theme';
@@ -9,6 +10,9 @@ function App() {
     <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
       <GlobalStyles />
+      <Helmet>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
+      </Helmet>
     </ThemeProvider>
   )
 }
